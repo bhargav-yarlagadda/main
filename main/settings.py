98cc60fd337f55c7754ddd88620bd2846295bbf1
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-10-12 20:42:38 +0530

    final

--- main/settings.py
+++ main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+EXTERNAL_APPS = [
+    'weatherApp',
+]
+INSTALLED_APPS += EXTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -111,13 +116,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- weatherApp/views.py
+++ weatherApp/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+from dotenv import load_dotenv
+import os
 
-# Create your views here.
+load_dotenv()
+
+def home_page(request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'hyderabad'
+    
+    # API Keys
+    API_KEY = os.getenv('DJANGO_WEATHER_API_KEY')  
+    UNSPLASH_API_KEY = os.getenv('UNSPLASH_API_KEY')  
+    
+    # OpenWeather API URL
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+    PARAMS = {'units': 'metric'}
+
+    # Unsplash API URL
+    unsplash_url = f'https://api.unsplash.com/search/photos?query={city}&client_id={UNSPLASH_API_KEY}'
+
+    # Get image URL
+    image_url = 'https://example.com/default-image.jpg'  
+
+    # Fetch Unsplash image
+    unsplash_response = requests.get(unsplash_url)
+    if unsplash_response.status_code == 200:
+        unsplash_data = unsplash_response.json()
+        if unsplash_data.get("results") and len(unsplash_data["results"]) > 0:
+            image_url = unsplash_data["results"][3]['urls']['regular']  
+    else:
+        messages.error(request, 'Failed to retrieve image data.')
+
+    # Fetch weather data
+    weather_response = requests.get(url, params=PARAMS)
+    if weather_response.status_code == 200:
+        weather_data = weather_response.json()
+        description = weather_data['weather'][0]['description']
+        icon = weather_data['weather'][0]['icon']
+        temp = weather_data['main']['temp']
+        day = datetime.date.today()
+
+        return render(request, 'home.html', {
+            'description': description,
+            'icon': icon,
+            'temp': temp,
+            'day': day,
+            'city': city,
+            'exception_occurred': False,
+            'image_url': image_url
+        })
+    else:
+        messages.error(request, 'Failed to retrieve weather data. Please check the city name.')
+        day = datetime.date.today()
+
+        return render(request, 'home.html', {
+            'description': 'clear sky',
+            'icon': '01d',
+            'temp': 27,
+            'day': day,
+            'city': city,
+            'exception_occurred': True,
+            'image_url': image_url
+        })
